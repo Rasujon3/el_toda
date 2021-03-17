c693db807a64bc7910de68282a58a4f42edbf590
sujon <rasujon3@gmail.com>
2021-03-17 19:58:52 +0600

    creating category form
    creating category form without save or cancel

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,3 +1,4 @@
+import 'package:el_toda/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatefulWidget {
@@ -6,10 +7,73 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            actions: [
+              FlatButton(
+                onPressed: (){},
+                child: Text("Save"),
+              ),//save
+              FlatButton(
+                onPressed: (){},
+                child: Text("Cancel"),
+              ),//cancel
+            ],
+            title: Text('Category form'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Category name',
+                      hintText: 'Write category name',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Category description',
+                      hintText: 'Write category description',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("welcome to categories screen"),),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("El Todo"),
+        leading: RaisedButton(
+          elevation: 0.0,
+          color: Colors.red,
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+                new MaterialPageRoute(builder: (context) => new HomeScreen()));
+          },
+        ),
+      ),
+      body: Center(
+        child: Text("Welcome to Category"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showFormDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
