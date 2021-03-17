accc3de475d86c72cd466de5e681e0888374b8ba
sujon <rasujon3@gmail.com>
2021-03-17 18:51:57 +0600

    64
    64

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:el_toda/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
